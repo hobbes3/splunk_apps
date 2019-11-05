5684af6dd4f3c3f7ec3544bf2df4dd8baff18884
Satoshi Kawasaki <hobbes3@gmail.com>
2019-11-04 17:15:07 -0800

    Updated install script to also include spl file extensions. Also manually split up the dashboard beta app.

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ AUTH=$1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 find "$DIR" -maxdepth 1 -name "*.tgz" -not -name "python-for-scientific-computing*" -exec splunk install app {} -update 1 -auth $AUTH \;
+# For some reason, -or won't work on my Mac.
+find "$DIR" -maxdepth 1 -name "*.spl" -not -name "python-for-scientific-computing*" -exec splunk install app {} -update 1 -auth $AUTH \;
 
 if [ "$(uname)" == "Darwin" ]; then
     find "$DIR" -maxdepth 1 -name "python-for-scientific-computing-for-mac*" -exec splunk install app {} -update 1 -auth $AUTH \;
